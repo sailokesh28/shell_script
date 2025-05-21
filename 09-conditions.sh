0515d42d2ed353409268004a989ff881c57b1848
SaiLokesh <sailokesheduru95@gmail.com>
2025-05-22 00:19:18 +0530

    conservation added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
     echo "Number is greater than 100"
-    else
-    echo "Number is less than or equal to 100"
-    else
-    echo "Number is less than 100"
+else
+    echo "Number is less than or equal to 100" 
 fi

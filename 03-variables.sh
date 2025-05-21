f2acfbe30a81b9382436793dbbd1bc24e6dccb67
SaiLokesh <sailokesheduru95@gmail.com>
2025-05-21 23:33:17 +0530

    conservation added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=RAMESH #NO SPACE BEFORE AND AFTER EQUALS
+PERSON2=SURESH
+
+echo "$PERSON1::  Hi $PERSON2"
+ echo "$PERSON2::Hello $PERSON1"
+ echo "$PERSON1 :: How are you?"
+echo "$PERSON2:: I am fine, what about you?"
+echo "$PERSON1:: I am also fine"
+echo "$PERSON2:: "Good"

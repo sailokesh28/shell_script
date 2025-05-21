32678bc70d1cc7c0aee01163f53d6fdd38bf6b99
SaiLokesh <sailokesheduru95@gmail.com>
2025-05-21 23:43:07 +0530

    conservation added

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -5,3 +5,7 @@ echo "Please enter your username::"  # text entered here will be added as value 
 read -s USERNAME  #here username is a variable name
 
 echo "Username entered : $USERNAME"
+
+echo "Please Entered Password::"  
+
+read -s PASSWORD  #here password is a variable name
